9e1de85dd3ebfd306efa6a6ce58325e05a961a84
Caleb Bartholomew <caleb@echobroadband.net>
2018-11-06 12:12:40 -0700

    Allowed run to execute commands with values that have spaces. Added verbose mode to run to see what the API command representation is.

--- examples/run/main.go
+++ examples/run/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"strings"
 
@@ -15,6 +16,7 @@ var (
 	password = flag.String("password", "admin", "Password")
 	async    = flag.Bool("async", false, "Use async code")
 	useTLS   = flag.Bool("tls", false, "Use TLS")
+	verbose  = flag.Bool("v", false, "Use verbose mode")
 )
 
 func dial() (*routeros.Client, error) {
@@ -37,7 +39,45 @@ func main() {
 		c.Async()
 	}
 
-	r, err := c.RunArgs(strings.Split(*command, " "))
+	cmdArray := []string{}
+
+	tmpCmd := *command
+	quoteIndex := strings.Index(tmpCmd, `"`)
+
+	for quoteIndex != -1 {
+		// Extract up to the first instance of quote in string
+		ss := tmpCmd[:quoteIndex]
+		// Split by spaces to get the command arguments
+		split := strings.Split(ss, " ")
+
+		// Get everything after the first quote
+		ss = tmpCmd[quoteIndex+1:]
+
+		// Get the next instance of quote
+		quoteIndex = strings.Index(ss, `"`)
+
+		// Append everything up to the second quotation
+		split[len(split)-1] += ss[:quoteIndex]
+
+		// Append to cmdArray
+		cmdArray = append(cmdArray, split...)
+
+		// tmCmd is everything after the second quotation mark
+		tmpCmd = ss[quoteIndex+1:]
+
+		// Get the next quotation mark index
+		quoteIndex = strings.Index(tmpCmd, `"`)
+	}
+
+	cmdArray = append(cmdArray, strings.Split(strings.Trim(tmpCmd, " "), " ")...)
+
+	if *verbose {
+		for _, cmd := range cmdArray {
+			fmt.Println(cmd)
+		}
+	}
+
+	r, err := c.RunArgs(cmdArray)
 	if err != nil {
 		log.Fatal(err)
 	}
